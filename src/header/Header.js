53b83e42e1261805a3b200a3b58f80fd1dfbef66
nsourov <naimur.rahman103@gmail.com>
2018-02-08 21:58:22 +0600

    Localstorage and already checked input box

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import './header.css';
+import React, { Component } from "react";
+import "./header.css";
 
 class Header extends Component {
-    render() {
-        const ref = this;
-        return (
-            <div className={ref.props.header ? "header new_header": "header"}>
-            	<div className="head">
-            		<div className="today">TODAY</div>
-            		<div className="task_open">
-            			<span>7</span> Open Task
-            		</div>
-            	</div>
-            </div>
-        );
-    }
+  render() {
+    const ref = this;
+    let open = [];
+    const todo = ref.props.todo.map(el => {
+      if (!el.completed) {
+          open.push(el)
+      }
+    });
+    return (
+      <div className={ref.props.header ? "header new_header" : "header"}>
+        <div className="head">
+          <div className="today">TODAY</div>
+          <div className="task_open">
+            <span>{open.length}</span> Open Task
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
